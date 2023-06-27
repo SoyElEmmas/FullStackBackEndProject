b3ae18cd8e2c79ef612afe3b09a2eb2da7335d30
Emmanuel Reyes <emmanuel.reyes.p@gmail.com>
2023-06-26 20:15:12 -0600

    modifciaccion de datos sensibles

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
 export default {
   NODE_ENV: 'development',
-  PORT: '5000',
-  MONGO_URI:
-    "mongodb+srv://emmanuelreyesp:gajOb2A5DPs1bSMS@practices01.n9ecvg6.mongodb.net/Movies.movies?retryWrites=true&w=majority",
-  JWT_SECRET: "espapirifactico",
+  PORT: '5000'
 };
 /*
 NODE_ENV = development
