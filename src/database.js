54781e800a3c38d947a4af800f6c2e75beeb9271
Emmanuel Reyes <emmanuel.reyes.p@gmail.com>
2023-06-28 10:13:28 -0600

    modificacion de coneccion a la BD

--- src/database.js
+++ src/database.js
@@ -5,18 +5,3 @@ dotenv.config()
 mongoose.connect(process.env.MONGO_URI)
 .then(db => console.log(`MongoDB Connected`) )
 .catch(error => console.error(error))
-
-/*const mongoose = require('mongoose')
-
-const connectDB = async () => {
-    try {
-        console.log('Mongo URI: '+process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1)
-    }
-}
-
-module.exports = connectDB*/ 
